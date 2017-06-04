4fbd63c31ac6cf5682a6640f7729a76dbc811298
erwinwahyura <eriwnwahyuramadhan@gmail.com>
2017-06-05 01:06:54 +0700

    initial index routing

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express')
 var router = express.Router()
 //controllers
+var todo_controller = require('../controllers/todo_controller')
 
 router.get('/', function(req, res ) {
   res.send({title: 'Welcome this is Express'})
 })
 
 //routing here
+router.get('/api/todos', todo_controller.getAll)
+router.get('/api/todos/:id', todo_controller.getById)
+router.post('/api/todos', todo_controller.create)
+router.put('/api/todos/:id', todo_controller.updates)
+router.delete('/api/todos/:id', todo_controller.remove)
 
 
 
