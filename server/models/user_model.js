d30845a9488999b2b2d94b0831871ca701e92804
erwinwahyura <eriwnwahyuramadhan@gmail.com>
2017-06-08 01:08:18 +0700

    add user model

---
+++ server/models/user_model.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var userSchema = new Schema ({
+  id: String,
+  name: String,
+  username: String,
+  password: String,
+  email: String,
+  createdDate: {
+    type: Date,
+    default: Date.now()
+  },
+  updatedDate: {
+    type: Date,
+    default: Date.now()
+  },
+  role: {
+    type: String,
+    default: 'user'
+  }
+})
+
+var user = mongoose.model('Users', userSchema)
+
+module.exports = user
